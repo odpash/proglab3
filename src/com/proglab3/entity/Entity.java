f4487d0e5cf43d6f78e503587c288ac9d3089c88
itookyourboo <Wignorbo@yandex.ru>
2020-11-27 22:28:20 +0300

    Place state

--- src/com/proglab3/entity/Entity.java
+++ src/com/proglab3/entity/Entity.java
@@ -1,6 +1,19 @@
 package com.proglab3.entity;
 
+import com.proglab3.place.Place;
+
 public abstract class Entity {
+    protected Place place = null;
+
     public abstract String getName();
+
     public abstract boolean canFly();
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
 }
